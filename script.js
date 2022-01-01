d44812f8b2fe14edbeceac16f977e601ffc0e074
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-01 19:31:13 -0300

    Add a new task complete

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.querySelector('#push').onclick = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
         alert(" Please enter a task")
+    } else {
+        document.querySelector('#tasks').innerHTML += `
+            <div div class="task" >
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                <i class="far fa-trash-alt"></i>
+                </button>
+            </div >
+            `
     }
 }
