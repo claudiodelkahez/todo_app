70034b631424c988f7520fe6f7c840a9d3eadb6a
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-01 19:45:33 -0300

    Crossing out a completed task

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+
 document.querySelector('#push').onclick = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
         alert(" Please enter a task")
+        //adding a task
     } else {
         document.querySelector('#tasks').innerHTML += `
             <div div class="task" >
@@ -11,6 +14,23 @@ document.querySelector('#push').onclick = function () {
                 <i class="far fa-trash-alt"></i>
                 </button>
             </div >
-            `
+            `;
+        //deleting a task
+        let current_tasks = document.querySelectorAll(".delete");
+        for (let i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            };
+
+        }
+        //crossing out a completed task
+        let tasks = document.querySelectorAll(".task");
+        for (let i = 0; i < tasks.length; i++) {
+            tasks[i].onclick = function () {
+                this.classList.toggle('completed');
+            };
+
+        }
+
     }
 }
